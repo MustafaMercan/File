7d446c1e2884feab53383677424ec4e9804043f0
MustafaMercan <mustfmrcann@gmail.com>
2024-01-05 21:10:15 +0300

    user auth controller added

--- server/controllers/authToken/userAuthController.js
+++ server/controllers/authToken/userAuthController.js
@@ -1,21 +1,27 @@
+const User = require('../../models/userModel');
 const jwt = require('jsonwebtoken')
+
 const { decrypt } = require('../../utils/utils')
+
 const userAuthController = async (req, res) => {
 
     let { token } = req.body;
+    console.log('token', token);
     const hashedToken = jwt.decode(token)?.token;
+    const userId = jwt.decode(token)?.id;
     if (hashedToken) {
-        console.log(decrypt(hashedToken));
-
-
+        const authUser = await User.findOne({authToken:token});
+        if(authUser){
+            console.log('db id -> ',authUser._id.toString());
+            console.log('client side id -> ',userId);
+            if(authUser._id.toString() === userId){
+                console.log('sa')
+                return res.status(200).json({message:"ok",authUserId:userId});
+            }  
+        }
     }
-    res.status(200);
-
-
-
+    return res.status(400).json({message:"try again xd"});
 }
-
-
 module.exports = {
     userAuthController
 }
